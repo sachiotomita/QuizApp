fe094f1ab0a5270321fcf9e5b1701cb079b673da
Arunda Victor <victoroyugi31@gmail.com>
2019-07-01 20:35:30 +0300

    added new constructor

--- app/src/main/java/com/example/quizapp/Question.java
+++ app/src/main/java/com/example/quizapp/Question.java
@@ -1,4 +1,18 @@
 package com.example.quizapp;
 
 public class Question {
+
+    private String question;
+    private String option1;
+    private String option2;
+    private String option3;
+    private int answer;
+
+    public Question(String question, String option1, String option2, String option3, int answer) {
+        this.question = question;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.answer = answer;
+    }
 }
