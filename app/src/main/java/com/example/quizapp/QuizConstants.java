323da023cb030d6e063f9d1222b84af2ea1d9643
Arunda Victor <victoroyugi31@gmail.com>
2019-07-01 21:29:48 +0300

    added various columns for the databse

--- app/src/main/java/com/example/quizapp/QuizConstants.java
+++ app/src/main/java/com/example/quizapp/QuizConstants.java
@@ -8,7 +8,7 @@ public final class QuizConstants {
 
     public static class QuestionsTable implements BaseColumns {
         public static final String TABLE_NAME = "quiz_questions";
-        public static final String COLUMN_UESTION = "question";
+        public static final String COLUMN_QUESTION = "question";
         public static final String COLUMN_OPTION1 = "option1";
         public static final String COLUMN_OPTION2 = "option2";
         public static final String COLUMN_OPTION3 = "option3";
--- app/src/main/java/com/example/quizapp/QuizDbHelper.java
+++ app/src/main/java/com/example/quizapp/QuizDbHelper.java
@@ -4,25 +4,40 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
+import com.example.quizapp.QuizConstants.*;
 
 public class QuizDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "QuizApp";
     private static final int DATABASE_VERSION = 1;
 
+    //reference to the actual database
     private SQLiteDatabase db;
 
-    public QuizDbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public QuizDbHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
+                QuestionsTable.TABLE_NAME + " ( " +
+                QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                QuestionsTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuestionsTable.COLUMN_ANSWER + " INTEGER" +
+                ")";
+
+
 
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase dp, int i, int i1) {
 
     }
 }
