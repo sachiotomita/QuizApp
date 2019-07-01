e6ff25e95cfeaf806ed8956c5de9fe2a17bd0181
Arunda Victor <victoroyugi31@gmail.com>
2019-07-01 21:02:47 +0300

    added new methods

--- app/src/main/java/com/example/quizapp/QuizDbHelper.java
+++ app/src/main/java/com/example/quizapp/QuizDbHelper.java
@@ -6,6 +6,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 
 public class QuizDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "QuizApp";
+    private static final int DATABASE_VERSION = 1;
+
+    private SQLiteDatabase db;
+
     public QuizDbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
