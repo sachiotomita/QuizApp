a90c77eb75e570ecbe8591824473862b5bfea594
Arunda Victor <victoroyugi31@gmail.com>
2019-07-02 13:37:03 +0300

    added new methods

--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.example.quizapp;
 
+import android.content.res.ColorStateList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -7,6 +8,7 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
@@ -23,6 +25,14 @@ public class QuizActivity extends AppCompatActivity {
     @BindView(R.id.button3) RadioButton Button3;
     @BindView(R.id.submitButton) Button submitButtonn;
 
+    private ColorStateList texxtColorDefault;
+    private int questionCounter;
+    private int questionTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
+
     private List<Question> questionList;
 
 
@@ -34,8 +44,46 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         ButterKnife.bind(this);
 
+        texxtColorDefault = Button1.getTextColors();
+
         //to create the database
         QuizDbHelper dbHelper = new QuizDbHelper(this);
         questionList = dbHelper.getAllQuestions();
+
+        questionTotal = questionList.size();
+        Collections.shuffle(questionList);
+
+        showNextQuestion();
+    }
+
+    private void showNextQuestion() {
+        Button1.setTextColor(texxtColorDefault);
+        Button2.setTextColor(texxtColorDefault);
+        Button3.setTextColor(texxtColorDefault);
+
+        radioGroupp.clearCheck();
+
+        //if there are more questions
+        if (questionCounter < questionTotal){
+
+            currentQuestion = questionList.get(questionCounter);
+            questionn.setText(currentQuestion.getQuestion());
+            Button1.setText(currentQuestion.getOption1());
+            Button2.setText(currentQuestion.getOption2());
+            Button3.setText(currentQuestion.getOption3());
+
+            questionCounter++;
+            questionCountt.setText("Question: " + questionCounter + "/" + questionTotal);
+            answered = false;
+            submitButtonn.setText("Confirm");
+
+
+        } else {
+            finishQuiz();
+        }
+    }
+
+    private void finishQuiz(){
+        finish();
     }
 }
