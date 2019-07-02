f4fb06c68d6005070b082077de15ba741ef1e5ae
Arunda Victor <victoroyugi31@gmail.com>
2019-07-03 01:07:08 +0300

    added countdown

--- app/src/main/java/com/example/quizapp/QuizActivity.java
+++ app/src/main/java/com/example/quizapp/QuizActivity.java
@@ -3,6 +3,7 @@ package com.example.quizapp;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,12 +15,14 @@ import android.widget.Toast;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class QuizActivity extends AppCompatActivity {
     public static final String EXTRA_SCORE = "extraScore";
+    private static final long COUNTDOWN_TIME = 30000;
 
     @BindView(R.id.score) TextView scorre;
     @BindView(R.id.questionCount) TextView questionCountt;
@@ -32,6 +35,11 @@ public class QuizActivity extends AppCompatActivity {
     @BindView(R.id.submitButton) Button finish;
 
     private ColorStateList texxtColorDefault;
+    private ColorStateList textColorDefaultcd;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeft;
+
     private int questionCounter;
     private int questionTotal;
     private Question currentQuestion;
@@ -53,6 +61,7 @@ public class QuizActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         texxtColorDefault = Button1.getTextColors();
+        textColorDefaultcd = countdownn.getTextColors();
 
         //to create the database
         QuizDbHelper dbHelper = new QuizDbHelper(this);
@@ -110,13 +119,54 @@ public class QuizActivity extends AppCompatActivity {
             finish.setText("Confirm");
 
 
+            //to start the countdown
+            timeLeft = COUNTDOWN_TIME;
+            startCountdown();
+
+
         } else {
             finishQuiz();
         }
     }
 
+    private void startCountdown(){
+        countDownTimer = new CountDownTimer(timeLeft, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeft = l;
+                updateCountDownText();
+
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeft = 0;
+                updateCountDownText();
+                checkAnswer();
+
+            }
+        }.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (timeLeft / 1000) / 60;
+        int seconds = (int) (timeLeft / 1000) % 60;
+
+        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        countdownn.setText(timeFormatted);
+
+        //when countdown gets to below 10
+        if (timeLeft < 10000) {
+            countdownn.setTextColor(Color.RED);
+        } else {
+            countdownn.setTextColor(textColorDefaultcd);
+        }
+    }
+
     private void checkAnswer(){
         answered = true;
+
+        countDownTimer.cancel();
 
         //get selected radio button
         RadioButton selected = findViewById(radioGroupp.getCheckedRadioButtonId());
@@ -181,5 +231,15 @@ public class QuizActivity extends AppCompatActivity {
             Toast.makeText(this, "Press back again to Exit", Toast.LENGTH_SHORT).show();
         }
         backPressedTime = System.currentTimeMillis();
+    }
+
+    //stop countdown once cancelled
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
     }
 }
